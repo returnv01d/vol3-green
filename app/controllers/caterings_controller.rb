333567dbe88c84a03df2b9736b9d3bdcb518374a
lemcur <loernittcc@gmail.com>
2018-08-24 16:51:57 +0200

    created controller and index for caterings (and a bit of routes)

---
+++ app/controllers/caterings_controller.rb
@@ -0,0 +1,31 @@
+class CateringsController < ApplicationController
+
+	def index
+		@caterings = Catering.all
+	end
+
+	def show
+		
+	end
+
+	def new
+		
+	end
+
+	def create
+		
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+
+end
