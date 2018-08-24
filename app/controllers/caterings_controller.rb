25e5fbb92ace1e26a8132b5ecd5d9814694955b9
lemcur <loernittcc@gmail.com>
2018-08-24 22:37:11 +0200

    most basic controller show and index

--- app/controllers/caterings_controller.rb
+++ app/controllers/caterings_controller.rb
@@ -5,7 +5,11 @@ class CateringsController < ApplicationController
 	end
 
 	def show
-		
+		begin @catering = Catering.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+			flash.now.alert = "This Catering does not exist"
+			redirect_to :index
+		end
 	end
 
 	def new
@@ -28,4 +32,15 @@ class CateringsController < ApplicationController
 
 	end
 
+	private
+
+	def catering_params
+		params.require(:catering).permit(:author, :content)
+	end
+	rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
+	def record_not_found
+		redirect_to caterings_path
+		flash[:alert] = "No such catering"
+	end
 end
