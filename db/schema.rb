e60fe036ff7a03aaafdb784bc6717f9f210e06f2
lemcur <loernittcc@gmail.com>
2018-08-24 14:43:52 +0200

    added catering model, migration and testwith rails helper addition

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,14 @@ ActiveRecord::Schema.define(version: 2018_08_24_120902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "caterings", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "city", null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
