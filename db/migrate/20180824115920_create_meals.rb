207dab0c379ede9c0c2578bae11aa8f3119cbd3b
Grzegorz <kompnet@gmail.com>
2018-08-24 14:53:11 +0200

    Added Meal model

---
+++ db/migrate/20180824115920_create_meals.rb
@@ -0,0 +1,10 @@
+class CreateMeals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :meals do |t|
+      t.string :name, null:false
+      t.belongs_to :catering, null:false
+      t.timestamps
+    end
+    # add_index :meals, #:id_catering
+  end
+end
