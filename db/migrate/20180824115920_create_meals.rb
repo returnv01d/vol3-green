ca026627a5799b5d6092ad0c14e2f755fcafb819
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-27 11:19:22 +0100

    deleted old migration

--- db/migrate/20180824115920_create_meals.rb
+++
@@ -1,9 +0,0 @@
-class CreateMeals < ActiveRecord::Migration[5.2]
-  def change
-    create_table :meals do |t|
-      t.string :name, null: false
-      t.timestamps
-    end
-    # add_index :meals, #:id_catering
-  end
-end
