67ff7b1ca4763fba29ac3a1bc6b171885bc0a222
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-27 11:26:01 +0100

    Deleted old Meal model file.

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,7 +1,2 @@
 class Meal < ApplicationRecord
-  belongs_to :catering
-  validates :name,
-    presence: true,
-    format: { with: /\A[a-zA-Z0-9]+\z/, message: 'only allows letters and numbers' }
-  validates :catering, presence: true 
 end
