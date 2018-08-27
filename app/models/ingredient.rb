9fc3b41e54bcc96259cc5f42141f7ca0a88d4f48
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-27 15:16:19 +0100

    Updated Ingredient model with 'meals' relation.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,9 @@ class Ingredient < ApplicationRecord
   has_and_belongs_to_many :allergic_users,
                           class_name: 'User' ,
                           foreign_key: 'ingredient_id',
-                          join_table: :user_allergies
+                          join_table: :users_allergies
+  has_and_belongs_to_many :meals,
+                          class_name: 'Meal' ,
+                          foreign_key: 'ingredient_id',
+                          join_table: :meals_ingredients
 end
