a5d516436e27f9a4436209c626ac108f5caf0ee2
Piotr G <pgorni@teknik.io>
2018-08-27 15:57:22 +0200

    Created views for MealsController; wrote basic business logic

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,15 +1,21 @@
 class MealsController < ApplicationController
+
+  before_action :authenticate_catering!
+
   def index
     # The catering service can list all the meals they offer
+    @meals = Meal.where(catering: current_catering)
   end
 
   def show
     # The catering service can display a specific meal
     # with some stats maybe?
     # Times ordered, last ordered, ...
+    @meal = Meal.find(params.require(:id))
   end
 
   def edit
+    # @meal = Meal.find(params[:id])
     # The catering service can edit a specific meal
     # (as in, its ingredients)
   end
