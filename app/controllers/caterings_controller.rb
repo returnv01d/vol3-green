ebd8384d278445ef49c421a7480dc2abcadd1087
lemcur <loernittcc@gmail.com>
2018-08-27 13:48:40 +0200

    decided to go for devise, no need to have create and update actions

--- app/controllers/caterings_controller.rb
+++ app/controllers/caterings_controller.rb
@@ -11,45 +11,11 @@ class CateringsController < ApplicationController
 		end
 	end
 
-	def new
-		@catering = Catering.new
-	end
-
-	def create
-		@catering = Catering.new(catering_params)
-		if @catering.save
-			redirect_to root_path
-			flash[:notice] = "Catering created"
-		else
-			redirect_to new_catering_path
-			flash[:alert] = "Try again!"
-		end
-	end
-
-	def edit
-		@catering = Catering.find_by_id(params[:id])
-	end
-
-	def update
-		@catering = Catering.find_by_id(params[:id])
-		if @catering.update(catering_params)
-			redirect_to root_path
-			flash[:notice] = "Catering updated"
-		else
-			redirect_to edit_catering_path
-			flash[:alert] = "Try again!"
-		end
-	end
-
 	private
 
 	def record_not_found
 		redirect_to root_path
 		flash[:alert] = "No such catering"
-	end
-
-	def catering_params
-    params.require(:catering).permit(:name, :city, :description)
 	end
 
 end
