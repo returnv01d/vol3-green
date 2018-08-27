c3f6909c82616c0c9d787e863be59998f900edf6
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-27 15:38:03 +0100

    Changed Ingredient model to connect with Diet table.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -7,4 +7,8 @@ class Ingredient < ApplicationRecord
                           class_name: 'Meal' ,
                           foreign_key: 'ingredient_id',
                           join_table: :meals_ingredients
+  has_and_belongs_to_many :diets,
+                          class_name: 'Diet' ,
+                          foreign_key: 'ingredient_id',
+                          join_table: :diets_ingredients
 end
