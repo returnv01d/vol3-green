a95dab06673b86679f375734b19447dfdf4d8565
lemcur <loernittcc@gmail.com>
2018-08-27 11:51:27 +0200

    adding other views (not styled) for catering

--- app/controllers/caterings_controller.rb
+++ app/controllers/caterings_controller.rb
@@ -12,23 +12,39 @@ class CateringsController < ApplicationController
 	end
 
 	def new
-		
+		@catering = Catering.new
 	end
 
 	def create
-		
+		@catering = Catering.new(catering_params)
+		if @catering.save
+			redirect_to root_path
+			flash[:notice] = "Catering created"
+		else
+			redirect_to new_catering_path
+			flash[:alert] = "Try again!"
+		end
 	end
 
 	def edit
-
+		@catering = Catering.find_by_id(params[:id])
 	end
 
 	def update
-
+		@catering = Catering.find_by_id(params[:id])
+		if @catering.update(catering_params)
+			redirect_to root_path
+			flash[:notice] = "Catering updated"
+		else
+			redirect_to edit_catering_path
+			flash[:alert] = "Try again!"
+		end
 	end
 
 	def destroy
-
+		@catering = Catering.find_by_id(params[:id])
+		@catering.destroy
+		flash[:notice] = "Catering deleted :'("
 	end
 
 	private
@@ -36,5 +52,9 @@ class CateringsController < ApplicationController
 	def record_not_found
 		redirect_to root_path
 		flash[:alert] = "No such catering"
+	end
+
+	def catering_params
+    params.require(:catering).permit(:name, :city, :description)
 	end
 end
