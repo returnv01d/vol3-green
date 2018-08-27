f037bd6cb5d2b097428626f3c8d2b83d186183a1
lemcur <loernittcc@gmail.com>
2018-08-27 11:55:23 +0200

    i don't want to modify master branch :c

--- app/controllers/caterings_controller.rb
+++ app/controllers/caterings_controller.rb
@@ -11,42 +11,6 @@ class CateringsController < ApplicationController
 		end
 	end
 
-	def new
-		@catering = Catering.new
-	end
-
-	def create
-		@catering = Catering.new(catering_params)
-		if @catering.save
-			redirect_to root_path
-			flash[:notice] = "Catering created"
-		else
-			redirect_to new_catering_path
-			flash[:alert] = "Try again!"
-		end
-	end
-
-	def edit
-		@catering = Catering.find_by_id(params[:id])
-	end
-
-	def update
-		@catering = Catering.find_by_id(params[:id])
-		if @catering.update(catering_params)
-			redirect_to root_path
-			flash[:notice] = "Catering updated"
-		else
-			redirect_to edit_catering_path
-			flash[:alert] = "Try again!"
-		end
-	end
-
-	def destroy
-		@catering = Catering.find_by_id(params[:id])
-		@catering.destroy
-		flash[:notice] = "Catering deleted :'("
-	end
-
 	private
 
 	def record_not_found
@@ -54,7 +18,4 @@ class CateringsController < ApplicationController
 		flash[:alert] = "No such catering"
 	end
 
-	def catering_params
-    params.require(:catering).permit(:name, :city, :description)
-	end
 end
