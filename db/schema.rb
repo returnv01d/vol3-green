99976d8df873c99ea3c4cad4b2c80f849603086e
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-28 11:41:46 +0200

    Updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -1,7 +1,18 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_135647) do
+ActiveRecord::Schema.define(version: 2018_08_28_091717) do
 
-
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "caterings", force: :cascade do |t|
@@ -40,6 +51,16 @@ ActiveRecord::Schema.define(version: 2018_08_27_135647) do
     t.bigint "ingredient_id"
     t.index ["diet_id"], name: "index_diets_ingredients_on_diet_id"
     t.index ["ingredient_id"], name: "index_diets_ingredients_on_ingredient_id"
+  end
+
+  create_table "food_requests", force: :cascade do |t|
+    t.boolean "is_autorequested", null: false
+    t.bigint "user_id"
+    t.bigint "daily_meal_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["daily_meal_id"], name: "index_food_requests_on_daily_meal_id"
+    t.index ["user_id"], name: "index_food_requests_on_user_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
