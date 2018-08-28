da5d2e8ff88bf5f9de0a453262ab7747468d477a
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-28 11:51:52 +0200

    Add default 'false' value to is_autorequested field.

--- db/migrate/20180828091717_create_food_requests.rb
+++ db/migrate/20180828091717_create_food_requests.rb
@@ -1,7 +1,7 @@
 class CreateFoodRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :food_requests do |t|
-      t.boolean :is_autorequested, null: false
+      t.boolean :is_autorequested, null: false, default: false
       t.belongs_to :user, index: true
       t.belongs_to :daily_meal, index: true
       t.timestamps
