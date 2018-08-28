09e6a9e4af06ed1cce3890009a1f636c0a36452e
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-28 23:36:27 +0200

    Add reverse behavior.

--- db/migrate/20180828094831_add_not_null_constraint_to_city_and_name_column_in_catering_model.rb
+++ db/migrate/20180828094831_add_not_null_constraint_to_city_and_name_column_in_catering_model.rb
@@ -1,6 +1,11 @@
 class AddNotNullConstraintToCityAndNameColumnInCateringModel < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column :caterings, :name, :string, null: false
     change_column :caterings, :city, :string, null: false
   end
+  def down
+    change_column :caterings, :name, :string
+    change_column :caterings, :city, :string
+  end
+
 end
