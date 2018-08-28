b8b79134b0cd238356b3a4656f4875bb45d9bcb4
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-28 23:36:58 +0200

    Updated db schema model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_094831) do
+ActiveRecord::Schema.define(version: 2018_08_28_191615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,13 @@ ActiveRecord::Schema.define(version: 2018_08_28_094831) do
     t.datetime "updated_at", null: false
     t.bigint "catering_id"
     t.index ["catering_id"], name: "index_meals_on_catering_id"
+  end
+
+  create_table "meals_diets", force: :cascade do |t|
+    t.bigint "meal_id"
+    t.bigint "diet_id"
+    t.index ["diet_id"], name: "index_meals_diets_on_diet_id"
+    t.index ["meal_id"], name: "index_meals_diets_on_meal_id"
   end
 
   create_table "meals_ingredients", force: :cascade do |t|
