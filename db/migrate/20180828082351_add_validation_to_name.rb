f1c5b7d1302c4cf6b86749a0e61b1051f73aee5e
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-28 23:36:05 +0200

    Add reverse behavior.

--- db/migrate/20180828082351_add_validation_to_name.rb
+++ db/migrate/20180828082351_add_validation_to_name.rb
@@ -1,5 +1,9 @@
 class AddValidationToName < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column :ingredients, :name, :string, unique: true, null: false
+  end
+
+  def down
+    change_column :ingredients, :name, :string, null: false
   end
 end
