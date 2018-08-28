a280dd1ba9cab8edbbc4dc7560befa7196ad1099
Piotr G <pgorni@teknik.io>
2018-08-28 17:06:18 +0200

    Caterings can now list their DailyMeals and create new ones
    This is a very naive implementation, but seems to be working for now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'daily_meals/index'
+  get 'daily_meals/new'
+  get 'daily_meals/create'
   resources :ingredients
   devise_for :caterings
   devise_for :users
   
   resources :caterings, only: [:index, :show] do
     resources :meals, only: [:index, :show, :edit, :new, :create]
+    resources :daily_meals, only: [:index, :new, :create]
   end
   root to: "welcome#index"
 end
