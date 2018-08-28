5ebe4f30e93cf21cce1e9f56db02ddd3a183d1de
Piotr G <pgorni@teknik.io>
2018-08-28 10:42:49 +0200

    Catering can now enter extra data during signup & on the profile edit page

--- app/sanitizers/catering_sanitizer.rb
+++ app/sanitizers/catering_sanitizer.rb
@@ -1,7 +1,7 @@
 class Catering::ParameterSanitizer < Devise::ParameterSanitizer
   def initialize(*)
     super
-    permit(:sign_up, keys: [:name, :city])
-    permit(:account_update, keys: [:name, :city])
+    permit(:sign_up, keys: [:name, :description, :city])
+    permit(:account_update, keys: [:name, :description, :city])
   end
 end
