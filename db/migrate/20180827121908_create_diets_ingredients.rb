5d15fd9e5a88ad7fc2d0c7993ca426c0961ba2f2
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-29 09:28:44 +0200

    Changed Ingredient creation to use find_or_create_by.

--- db/migrate/20180827121908_create_diets_ingredients.rb
+++
@@ -1,9 +0,0 @@
-class CreateDietsIngredients < ActiveRecord::Migration[5.2]
-  def change
-    create_table :diets_ingredients do |t|
-      t.belongs_to :diet, index: true
-      t.belongs_to :ingredient, index: true
-    end
-
-  end
-end
