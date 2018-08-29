76832d6349c9c86c91e8066439e439a4fce064a1
Grzegorz <kompnet@gmail.com>
2018-08-29 14:09:52 +0200

    Create routes for User allergie's form

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new_allergic_prefs'
   get 'users/create_allergic_prefs'
   get 'users/edit_allergic_prefs'
   get 'users/delete_allergic_prefs'
   resources :ingredients
   devise_for :caterings
   devise_for :users
-  
+
   resources :caterings, only: [:index, :show] do
     resources :meals, only: [:index, :show, :edit, :new, :create]
     resources :daily_meals, only: [:index, :new, :create]
