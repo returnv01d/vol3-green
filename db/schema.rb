3e73c0bd82f2f92eae0b979b8ed42c85af62a39b
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-30 10:43:22 +0200

    Schema update.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_191615) do
+ActiveRecord::Schema.define(version: 2018_08_29_202428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_08_28_191615) do
     t.string "name", null: false
     t.string "city", null: false
     t.text "description"
+    t.string "food_order_close_time", default: "10:00", null: false
     t.index ["email"], name: "index_caterings_on_email", unique: true
     t.index ["reset_password_token"], name: "index_caterings_on_reset_password_token", unique: true
   end
