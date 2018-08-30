615fd8a3b5b50a4f1366a2a285a9d8baabf202e7
lemcur <loernittcc@gmail.com>
2018-08-30 10:08:10 +0200

    changed what was to be changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only:[:edit_diet, :update_diet]
+  before_action :authenticate_user!, only: [:edit_diet, :update_diet]
   def create_allergic_prefs
   end
 
@@ -22,11 +22,10 @@ class UsersController < ApplicationController
     current_user.diets.clear()
     if current_user.diets << Diet.find_by_name(@diet)
       flash[:notice] = "Your diet is now #{@diet}"
-      redirect_to users_edit_diet_path
     else
       flash[:alert] = "Something went wrong! :C"
-      redirect_to users_edit_diet_path
     end
+    redirect_to users_edit_diet_path
   end
 
 end
