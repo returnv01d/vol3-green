4ce4ebed90039b98c680f5153b77921a0c997311
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-08-30 12:59:29 +0200

    Start job after Catering record update.Moved start job code to method (there is a better word for this but i dont remember at this moment :/).

--- app/models/catering.rb
+++ app/models/catering.rb
@@ -9,24 +9,33 @@ class Catering < ApplicationRecord
                           class_name: 'User' ,
                           foreign_key: 'catering_id',
                           join_table: :subscriptions
+
   after_create do |catering|
-    now = DateTime.now
-    close_minute = get_food_order_close_time[:min]
-    close_hour = get_food_order_close_time[:hour]
+    queue_food_order_job(catering.id)
+  end
 
-    if now.hour > close_hour && now.min > close_minute
-      job_date = DateTime.now.tomorrow
-      job_date.min = close_minute
-      job_date.hour = close_hour
-    else
-      job_date = DateTime.now.change({hour: close_hour, min: close_minute, sec: 0 })
-    end
+  after_update do |catering|
+    queue_food_order_job catering.id
 
-    OrderMealForUsersJob.set(wait_until: job_date).perform_later(catering.id)
   end
 
   def get_food_order_close_time
     time = self.food_order_close_time.split(':')
     {hour: time[0].to_i, min: time[1].to_i}
   end
+
+  def queue_food_order_job catering_id
+    now = DateTime.now
+    close_min = get_food_order_close_time[:min]
+    close_hour = get_food_order_close_time[:hour]
+
+    if now.hour > close_hour and now.min > close_min
+      job_date = DateTime.now.tomorrow.change({hour: close_hour, min: close_min, sec: 0 })
+    else
+      job_date = DateTime.now.change({hour: close_hour, min: close_min, sec: 0 })
+    end
+
+    OrderMealForUsersJob.set(wait_until: job_date).perform_later(catering_id)
+  end
+
 end
